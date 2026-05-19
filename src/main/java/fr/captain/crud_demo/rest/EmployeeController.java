4146c97605b58582ad16db85b9924c31bb3a637d
altaris33 <jeremie.laera@live.fr>
2026-05-20 01:42:14 +0200

    Patch : added partial updates using HTTP Patch

--- src/main/java/fr/captain/crud_demo/rest/EmployeeController.java
+++ src/main/java/fr/captain/crud_demo/rest/EmployeeController.java
@@ -3,16 +3,21 @@ package fr.captain.crud_demo.rest;
 import fr.captain.crud_demo.entity.Employee;
 import fr.captain.crud_demo.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
+    private final JsonMapper jsonMapper;
     private final EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(JsonMapper jsonMapper, EmployeeService employeeService) {
+        this.jsonMapper = jsonMapper;
         this.employeeService = employeeService;
     }
 
@@ -20,7 +25,7 @@ public class EmployeeController {
     public Employee getEmployeeById(@PathVariable int id) {
         Employee employee = employeeService.getEmployeeById(id);
         if (employee == null) {
-            throw new RuntimeException("Employee not found: " + employee);
+            throw new RuntimeException("Employee id not found: " + id);
         }
         return employeeService.getEmployeeById(id);
     }
@@ -38,6 +43,21 @@ public class EmployeeController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee employee) {
         return employeeService.save(employee);
+    }
+
+    @PatchMapping("/employees/{id}")
+    public Employee patchEmployee(@PathVariable int id, @RequestBody Map<String, Object> payload) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found: " + id);
+        }
+
+        if (payload.containsKey("id")) {
+            throw new RuntimeException("Request Body cannot contain an id: " + id);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(employee, payload);
+        return employeeService.save(patchedEmployee);
     }
 
     @DeleteMapping("/employees/{id}")
