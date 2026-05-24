ce89816d0a2f58c95ce9ceb9b73cf63a1034609b
altaris33 <jeremie.laera@live.fr>
2026-05-24 16:10:53 +0200

    Added API documentation using Open API and Swagger

--- src/main/java/fr/captain/crud_demo/dao/EmployeeRepository.java
+++ src/main/java/fr/captain/crud_demo/dao/EmployeeRepository.java
@@ -2,8 +2,6 @@ package fr.captain.crud_demo.dao;
 
 import fr.captain.crud_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
